5d7edc0950603fab1b067025c01d3747518c4dcc
uzun <hyjhyj0901@pusan.ac.kr>
2022-08-25 16:51:31 +0900

    notion root id changed

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7d6636dc20474540afe4cbaea572cb66~',
+  rootNotionPageId: '8d79d86aeaea4c779f26b609a936a971',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
